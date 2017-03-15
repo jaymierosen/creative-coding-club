e03db7be971e80897ff28e792797c161eca1ae23
Jaymie Rosen <rosen.jaymie@gmail.com>
2017-03-14 23:11:49 -0400

    working out beer animation with js

--- st-patricks-day/scripts.js
+++ st-patricks-day/scripts.js
@@ -1,6 +1,21 @@
-var fillButton = document.getElementById('fill');
-var beerLiquid = document.getElementsByTagName('body');
-fillButton.addEventListener('click', function() {
-	beerLiquid.classList.add('activate');
-	console.log(beerLiquid);
+const beerLiquid = document.getElementById('beer-liquid');
+const fillBtn = document.getElementById('fill-btn');
+const greenBtn = document.getElementById('green-btn');
+const clearBtn = document.getElementById('clear');
+
+const fillMeUp = function() {
+	beerLiquid.classList.add('beer-fill');
+	if (fillBtn.onclick) {
+		beerLiquid.style.background = '#D29D1D';
+	} else if (greenBtn.onclick) {
+		beerLiquid.style.background = '#7FFF00';
+	}
+}
+const clear = function() {
+	document.getElementById('beer-liquid').classList.remove('beer-fill');
+}
+//document ready
+document.addEventListener('DOMContentLoaded', function(event) {
+	clearBtn.addEventListener('click', clear);
 });
+//end of document ready
