bcb2b0d761854ce4849e76843233f6b39b6a4aa9
Jaymie Rosen <rosen.jaymie@gmail.com>
2017-03-17 00:03:29 -0400

    need to work on getting foam

--- st-patricks-day/scripts.js
+++ st-patricks-day/scripts.js
@@ -2,20 +2,35 @@ const beerLiquid = document.getElementById('beer-liquid');
 const fillBtn = document.getElementById('fill-btn');
 const greenBtn = document.getElementById('green-btn');
 const clearBtn = document.getElementById('clear');
+const foamBtn = document.getElementById('foam-btn');
+// Get desired elements
+const foam = document.getElementsByClassName('bubble');
 
-const fillMeUp = function() {
+const fillMeUpGold = function() {
 	beerLiquid.classList.add('beer-fill');
-	if (fillBtn.onclick) {
-		beerLiquid.style.background = '#D29D1D';
-	} else if (greenBtn.onclick) {
-		beerLiquid.style.background = '#7FFF00';
-	}
+	beerLiquid.style.background = '#D29D1D';
+}
+const fillMeUpGreen = function() {
+	beerLiquid.classList.add('beer-fill');
+	beerLiquid.style.background = '#669D31';
 }
 const clear = function() {
-	document.getElementById('beer-liquid').classList.remove('beer-fill');
+	beerLiquid.classList.remove('beer-fill');
 }
-//document ready
+// const foamAnim = function() {
+// 	// Iterate through the retrieved elements and add the necessary class names.
+// 	for(var i = 0; i < foam.length; i++)
+// 	{
+// 		foam[i].classList.add('foamAction');
+// 	}
+// }
+
+// document ready
 document.addEventListener('DOMContentLoaded', function(event) {
+	fillBtn.addEventListener('click', fillMeUpGold);
+	greenBtn.addEventListener('click', fillMeUpGreen);
 	clearBtn.addEventListener('click', clear);
+	// foamBtn.addEventListener('click', foamAnim);
+	// fillBtn.addEventListener('animationend', foamAnim, true);
 });
-//end of document ready
+// end of document ready
